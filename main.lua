41140f045409dc4ba0342b5eb83b8a5251b8d945
Matthias Gst <matthias.gaste@epitech.eu>
2023-02-10 23:19:16 +0100

    ADD basic file to start

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
 function love.load()
     love.window.setMode(1920, 1080)
     love.window.setTitle("Jam Super Héros")
+    love.graphics.setBackgroundColor(255/255, 200/255, 40/255, 127/255)
+    myImage = love.graphics.newImage("Pictures/luffy.png")
+end
+
+function love.draw()
+    love.graphics.draw(myImage, 100, 100)
+end
+
+function display_text()
+    love.graphics.draw("text")
 end
