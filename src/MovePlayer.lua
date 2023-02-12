2d838fee59f7b5a226e30ed6882d6aaf2e2a7567
Alban Peralta <alban.peralta@epitech.eu>
2023-02-12 18:45:38 +0100

    feat(MovePlayer):reset ground

--- src/MovePlayer.lua
+++ src/MovePlayer.lua
@@ -1,18 +1,20 @@
 require "src/IsTouch"
 require "src/PlayerCustomization"
 
+Rotate = 0
+
 MovePlayer = {}
 
 MovePlayer.speed = 400
 
 function MovePlayer.ReSpawn()
     player.x = 1920 / 2
-    player.y = 1080 - 135
+    player.y = 1080 - 150
     leftClickActive = false
 end
 
 function MovePlayer.Load()
-    player = {x = 1920 / 2, y = 1080 - 135, size = 80}
+    player = {x = 1920 / 2, y = 1080 - 150, size = 80}
     stick = {x = player.x, y = player.y - player.size, size = 50, angle = 0}
     leftClickActive = false
 end
@@ -33,9 +35,11 @@ function MovePlayer.Move(dt)
     if leftClickActive == true then
         player.x = player.x + MovePlayer.speed * player.v.x * dt
         player.y = player.y + MovePlayer.speed * player.v.y * dt
+        player.state = stateType.jump
         if IsTouch() == true then
             player.x = player.x - MovePlayer.speed * player.v.x * dt
             player.y = player.y - MovePlayer.speed * player.v.y * dt
+            player.state = stateType.ground
             leftClickActive = false
         end
     end
@@ -43,10 +47,10 @@ end
 
 function MovePlayer.SetDirection(x, y, btn)
     local dt = love.timer.getDelta()
-
     if btn == 2 then
         if not leftClickActive then
             leftClickActive = true
+            Rotate = math.atan2(y - player.y, x - player.x)
             player.v = { x = math.cos(stick.angle), y = math.sin(stick.angle) }
         end
     end
